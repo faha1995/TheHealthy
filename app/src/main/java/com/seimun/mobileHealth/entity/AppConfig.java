a5aa2df66e3dae885b54103552e1477b68761d76
mq-d-lufei <mq_d_lufei@sina.com>
2016-06-30 16:10:12 +0800

    注册、忘记密码、修改密码

--- app/src/main/java/com/seimun/mobileHealth/entity/AppConfig.java
+++ app/src/main/java/com/seimun/mobileHealth/entity/AppConfig.java
@@ -6,12 +6,22 @@ package com.seimun.mobileHealth.entity;
  */
 public class AppConfig {
 
+    //BaseUrl
+    public static String BASE_URL = "http://124.248.40.142/backend/";
+
     // Server user login url
     public static String URL_LOGIN = "http://124.248.40.142/backend/login/";
 
     // Server user register url
     public static String URL_REGISTER = "http://124.248.40.142/backend/register/";
     public static String URL_PERSONAL_INFO = "http://124.248.40.142/backend/personal_info/";
+    //new register url
+    public static String URL_REGISTER_NEW = BASE_URL + "register_new/";
+
+    //get verification code
+    public static String URL_GET_VERIFICATION_CODE = BASE_URL + "get_verification_code/";
+    //reset password
+    public static String URL_RESET_PASSWORD = BASE_URL + "reset_password/";
 
     // url to get service list
     public static String URL_SUMMARYS = "http://124.248.40.142/backend/summaries/";
